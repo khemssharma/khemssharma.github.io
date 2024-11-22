e402f36e2bd3c5cf1c7f26d72c792b72740c7156
Ayush Sharma <ayushsharma3310@gmail.com>
2024-11-22 23:24:38 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -45,7 +45,7 @@ const Navbar = () => {
           <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex '>
             Ayush Sharma &nbsp;
-            <span className='sm:block hidden'> | JavaScript Master</span>
+            <span className='sm:block hidden'> | Expert Web Developer</span>
           </p>
         </Link>
 
