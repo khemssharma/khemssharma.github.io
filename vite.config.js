3a914098bcd4d4b32be0549046407d840dee810b
Ayush Sharma <ayushsharma3310@gmail.com>
2025-12-24 03:54:22 +0530

    base: root

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/khemssharma.github.io/",
+  // For a user site hosted at https://<username>.github.io/, use root base
+  base: "/",
 })
