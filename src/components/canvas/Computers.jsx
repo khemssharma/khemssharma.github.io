f5bacf2879c0da7ede6bc3d3c2f6a917b5320bbd
Ayush Sharma <ayushsharma3310@gmail.com>
2025-12-26 20:12:40 +0530

    fix: dark computer

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -9,7 +9,9 @@ const Computers = ({ isMobile }) => {
 
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor='black' />
+      {/* Make the model visible on darker mobile displays by increasing ambient lighting */}
+      <hemisphereLight intensity={isMobile ? 0.35 : 0.15} groundColor='black' />
+      <ambientLight intensity={isMobile ? 0.6 : 0.3} />
       <spotLight
         position={[-20, 50, 10]}
         angle={0.12}
@@ -18,7 +20,7 @@ const Computers = ({ isMobile }) => {
         castShadow
         shadow-mapSize={1024}
       />
-      <pointLight intensity={1} />
+      <pointLight intensity={isMobile ? 1.25 : 1} />
       <primitive
         object={computer.scene}
         // Reduce scale on mobile so the model fits the viewport
@@ -77,6 +79,9 @@ const ComputersCanvas = () => {
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
           enableZoom={false}
+          enablePan={false}
+          autoRotate={true}
+          autoRotateSpeed={1.0}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
